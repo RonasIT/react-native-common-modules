4fa4241e83e68fb994aa77039ced84e93d3d6075
vmikhalevskii <vmikhalevskii@ronasit.com>
2024-06-07 10:43:40 +0300

    feat: use generator repo-config

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   resolver: '@nx/jest/plugins/resolver',
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
   moduleNameMapper: {
     '\\.svg$': '@nx/expo/plugins/jest/svg-mock',
   },
@@ -17,5 +17,4 @@ module.exports = {
     '^.+.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$':
       require.resolve('jest-expo/src/preset/assetFileTransformer.js'),
   },
-  coverageDirectory: '../../coverage/apps/mobile',
 };
