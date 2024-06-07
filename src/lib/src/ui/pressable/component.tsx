a59473ad4996ff8d39e2eb646bef1dd8f9d1cdcf
vmikhalevskii <vmikhalevskii@ronasit.com>
2024-06-07 13:32:28 +0300

    fix: type for AppPressable

--- src/lib/src/ui/pressable/component.tsx
+++ src/lib/src/ui/pressable/component.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Pressable, PressableProps, StyleSheet, StyleProp, ViewStyle } from 'react-native';
 
 export interface AppPressableProps extends PressableProps {
-  style?: StyleProp<ViewStyle>;
   pressedOpacity?: number;
 }
 
 export function AppPressable({ children, style, pressedOpacity = 0.4, ...props }: AppPressableProps) {
   return (
     <Pressable
-      style={({ pressed }) => StyleSheet.flatten([{ opacity: pressed ? pressedOpacity : 1 }, style])}
+      style={({ pressed }) =>
+        StyleSheet.flatten([{ opacity: pressed ? pressedOpacity : 1 }, style as StyleProp<ViewStyle>])
+      }
       {...props}>
       {children}
     </Pressable>
