9098db5923a955037950d056614cd929cc79d586
vmikhalevskii <vmikhalevskii@ronasit.com>
2024-07-08 10:17:30 +0300

    fix: add errors for constants

--- src/lib/src/data-access/store/utils/reactotron/builder.ts
+++ src/lib/src/data-access/store/utils/reactotron/builder.ts
@@ -7,8 +7,21 @@ export const setupReactotron = (
 ): (ReactotronReactNative & { createEnhancer: () => StoreEnhancer }) | undefined => {
   if (__DEV__) {
     const AsyncStorage = require('@react-native-async-storage/async-storage').default;
+    if (!AsyncStorage) {
+      throw new Error(
+        'AsyncStorage is undefined. Please ensure @react-native-async-storage/async-storage is properly installed.'
+      );
+    }
+
     const Reactotron: ReactotronReactNative = require('reactotron-react-native').default;
+    if (!Reactotron) {
+      throw new Error('Reactotron is undefined. Please ensure reactotron-react-native is properly installed.');
+    }
+
     const { reactotronRedux } = require('reactotron-redux');
+    if (!reactotronRedux) {
+      throw new Error('reactotronRedux is undefined. Please ensure reactotron-redux is properly installed.');
+    }
 
     return Reactotron.configure({ name: projectName })
       .setAsyncStorageHandler(AsyncStorage)
