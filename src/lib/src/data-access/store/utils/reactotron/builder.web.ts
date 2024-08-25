74b9ab05c0e2e5b91d20c3b4e709b4b179c50212
Ilya Pakhomov <ipakhomov@ronasit.com>
2024-08-25 21:43:07 +0600

    feat: added RN web alternative for reactotron

---
+++ src/lib/src/data-access/store/utils/reactotron/builder.web.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+export const setupReactotron = (projectName: string) => {
+  if (__DEV__) {
+    const Reactotron = require('reactotron-react-js').default;
+    if (!Reactotron) {
+      throw new Error('Reactotron for web is undefined. Please ensure reactotron-react-js is properly installed.');
+    }
+
+    const { reactotronRedux } = require('reactotron-redux');
+    if (!reactotronRedux) {
+      throw new Error('reactotronRedux is undefined. Please ensure reactotron-redux is properly installed.');
+    }
+
+    return Reactotron.configure({ name: projectName }).use(reactotronRedux()).connect();
+  }
+
+  return undefined;
+};
