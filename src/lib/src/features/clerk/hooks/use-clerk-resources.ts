ac49739543de13ddf722f9fff8d1350a0cb9fdc4
Vasily Eliseev <veliseev@ronasit.com>
2025-05-03 14:17:06 +0600

    fix: fix clerk use resources sign out func

--- src/lib/src/features/clerk/hooks/use-clerk-resources.ts
+++ src/lib/src/features/clerk/hooks/use-clerk-resources.ts
@@ -2,10 +2,10 @@ import { getClerkInstance, useAuth } from '@clerk/clerk-expo';
 import { UseClerkResourcesReturn } from '../types';
 
 export const useClerkResources = (): UseClerkResourcesReturn => {
-  const { signOut } = useAuth();
   const clerk = getClerkInstance();
   const signUp = clerk.client?.signUp;
   const signIn = clerk.client?.signIn;
+  const signOut = clerk.signOut;
   const setActive = clerk.setActive;
 
   return { signUp, signIn, setActive, signOut };
