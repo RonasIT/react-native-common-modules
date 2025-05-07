9b2e8c4908d47454a0cf85d1ab36840658defbf3
Vasily Eliseev <veliseev@ronasit.com>
2025-05-07 20:27:27 +0600

    fix: remove deleted imports

--- src/lib/src/features/clerk/hooks/index.ts
+++ src/lib/src/features/clerk/hooks/index.ts
@@ -1,5 +1,3 @@
-export * from './use-auth-with-phone-otp';
-export * from './use-auth-with-email-otp';
 export * from './use-auth-with-otp';
 export * from './use-clerk-resources';
 export * from './use-get-token';
