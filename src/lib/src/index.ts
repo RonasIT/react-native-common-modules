4fb9b013a4f0be1b4b85fc32383c0f1013b809e0
vmikhalevskii <vmikhalevskii@ronasit.com>
2025-07-08 17:18:53 +0300

    feat: add new imports for modules

---
+++ src/lib/src/index.ts
@@ -0,0 +1,6 @@
+// Required stub file
+// This package uses subpath exports
+// Import specific modules like:
+// import { AppSafeAreaView } from '@ronas-it/react-native-common-modules/safe-area-view';
+
+export {};
